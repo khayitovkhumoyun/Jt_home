0bb1046605247fa0024ba44a800468d9349e29f1
admin01 <khayitovkhumoyun000@gmail.com>
2025-10-09 12:47:11 +0500

    02

--- config/settings.py
+++ config/settings.py
@@ -71,8 +71,9 @@ TEMPLATES = [
 ]
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3003",
-    "http://172.22.0.43",
-    "https://172.22.0.43",
+    "http://172.22.0.43:8001",
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
     "https://front.uzswlu.uz/",
 ]
 CORS_ALLOW_ALL_ORIGINS = True
